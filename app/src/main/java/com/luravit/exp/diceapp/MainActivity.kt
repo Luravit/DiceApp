90c86d8e99d866fb7d7206b24a8624ddea9f9000
Vladislav <vladyslavchernobai@gmail.com>
2022-01-09 03:03:43 +0200

    Create another 3 dices connect dices present dices when random num equals numbers

--- app/src/main/java/com/luravit/exp/diceapp/MainActivity.kt
+++ app/src/main/java/com/luravit/exp/diceapp/MainActivity.kt
@@ -9,12 +9,18 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
 
     lateinit var diceOneImg : ImageView
+    lateinit var diceTwoImg : ImageView
+    lateinit var diceThreeImg : ImageView
+    lateinit var diceFourImg : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        diceOneImg = findViewById(R.id.img_dice)
+        diceOneImg = findViewById(R.id.img_dice1)
+        diceTwoImg = findViewById(R.id.img_dice2)
+        diceThreeImg = findViewById(R.id.img_dice3)
+        diceFourImg = findViewById(R.id.img_dice4)
 
         val rollButton = findViewById<Button>(R.id.btnRoll)
 
@@ -25,9 +31,13 @@ class MainActivity : AppCompatActivity() {
     }
     fun rollDice() {
 
-        val randomNumber = (1..6).random()
+        val randomNumber1 = (1..6).random()
+        val randomNumber2 = (1..6).random()
+        val randomNumber3 = (1..6).random()
+        val randomNumber4 = (1..6).random()
 
-        val imageRandom = when(randomNumber) {
+
+        val imageRandom1 = when(randomNumber1) {
             1 -> R.drawable.dice1
             2 -> R.drawable.dice2
             3 -> R.drawable.dice3
@@ -36,7 +46,44 @@ class MainActivity : AppCompatActivity() {
             6 -> R.drawable.dice6
         else -> R.drawable.dice_empty
         }
-diceOneImg.setImageResource(imageRandom)
+
+        val imageRandom2 = when(randomNumber2) {
+            1 -> R.drawable.dice1
+            2 -> R.drawable.dice2
+            3 -> R.drawable.dice3
+            4 -> R.drawable.dice4
+            5 -> R.drawable.dice5
+            6 -> R.drawable.dice6
+        else -> R.drawable.dice_empty
+        }
+
+
+        val imageRandom3 = when(randomNumber3) {
+            1 -> R.drawable.dice1
+            2 -> R.drawable.dice2
+            3 -> R.drawable.dice3
+            4 -> R.drawable.dice4
+            5 -> R.drawable.dice5
+            6 -> R.drawable.dice6
+        else -> R.drawable.dice_empty
+        }
+
+
+        val imageRandom4 = when(randomNumber4) {
+            1 -> R.drawable.dice1
+            2 -> R.drawable.dice2
+            3 -> R.drawable.dice3
+            4 -> R.drawable.dice4
+            5 -> R.drawable.dice5
+            6 -> R.drawable.dice6
+        else -> R.drawable.dice_empty
+        }
+
+
+diceOneImg.setImageResource(imageRandom1)
+diceTwoImg.setImageResource(imageRandom2)
+diceThreeImg.setImageResource(imageRandom3)
+diceFourImg.setImageResource(imageRandom4)
     }
 
 }
